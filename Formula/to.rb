1c5007de9e9b04a4dea95f600ef0b223d09cb242
Kasper <kasperkh.kh@gmail.com>
2019-04-25 01:19:22 +0200

    to had incorrect SHA256

--- Formula/to.rb
+++ Formula/to.rb
@@ -2,7 +2,7 @@ class To < Formula
   desc "Pretty simple bash script that converts audio, video and image files using FFmpeg and ImageMagick"
   homepage "https://github.com/spectralkh/to"
   url "https://github.com/SpectralKH/to/raw/master/dist/to-1.0.0.tar.gz"
-  sha256 "2dc0d4d5e5de7b4ab9a7dfde26a910e9f02d06fdd1492d5e0a95f7ba9e01fc3b"
+  sha256 "f7fc6e84b08136bed727bba4d002e4ed61975c09888903818a6ac4d1d355e6b5"
 
   depends_on "ffmpeg" => :recommended
   depends_on "imagemagick" => :recommended
