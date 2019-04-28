b3613cfe40b1820ebc078c8b875f70f8b370febe
Kasper <kasperkh.kh@gmail.com>
2019-04-28 02:22:16 +0200

    Update to.rb

--- Formula/to.rb
+++ Formula/to.rb
@@ -1,8 +1,8 @@
 class To < Formula
   desc "Pretty simple bash script that converts audio, video and image files using FFmpeg and ImageMagick"
   homepage "https://github.com/spectralkh/to"
-  url "https://github.com/SpectralKH/to/raw/master/dist/to-1.0.0.tar.gz"
-  sha256 "f7fc6e84b08136bed727bba4d002e4ed61975c09888903818a6ac4d1d355e6b5"
+  url "https://github.com/SpectralKH/to/raw/master/dist/to-1.0.1.tar.gz"
+  sha256 "cfd20dd5c1732edd125af050fcbefed91c5255b598efed564e08c8e236acede0"
 
   depends_on "ffmpeg" => :recommended
   depends_on "imagemagick" => :recommended
