af180f10c477368aa0ddb9ddbdba40797355348e
Kasper <kasperkh.kh@gmail.com>
2019-04-29 14:21:28 +0200

    Put version in it's own field

--- Formula/to.rb
+++ Formula/to.rb
@@ -1,7 +1,8 @@
 class To < Formula
   desc "Pretty simple bash script that converts audio, video and image files using FFmpeg and ImageMagick"
   homepage "https://github.com/spectralkh/to"
-  url "https://github.com/SpectralKH/to/raw/master/dist/to-1.1.0.tar.gz"
+  version "1.1.0"
+  url "https://github.com/SpectralKH/to/raw/master/dist/to-#{version}.tar.gz"
   sha256 "c16561829dda8ebfe8423ecf7f900ccdc97535b09a2d6e311782c2198c643009"
 
   depends_on "ffmpeg" => :recommended
