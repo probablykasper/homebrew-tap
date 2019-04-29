9b657313ba93ce4908e599fead754d631bb98719
Kasper <kasperkh.kh@gmail.com>
2019-04-29 22:13:37 +0200

    Add flutter 1.2.1

---
+++ Cask/flutter.rb
@@ -0,0 +1,10 @@
+cask "flutter" do
+  version "1.2.1"
+  sha256 '74ac8397ea29720f116980ea00cf60c34430be1f64489b407f7cf95553babbef'
+
+  url "https://storage.googleapis.com/flutter_infra/releases/stable/macos/flutter_macos_v#{version}-stable.zip"
+  name "flutter"
+  homepage "https://flutter.io"
+
+  binary "flutter/bin/flutter", target: "flutter"
+end
--- README.md
+++ README.md
@@ -1,5 +1,19 @@
 # homebrew-tap
-### Add this tap
+Add this tap by running:
 ```
 brew tap spectralkh/tap
 ```
+# Formulae
+
+### to
+https://github.com/SpectralKH/to
+
+
+# Casks
+Install casks by running:
+```
+brew cask install <name>
+```
+
+### flutter
+Unofficial Flutter formula (pre-compiled)
