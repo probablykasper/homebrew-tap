f3bf34364914b1e645450a6457290d6caee81580
Kasper <kasperkh.kh@gmail.com>
2019-08-10 20:25:58 +0200

    to 1.3.1

--- Casks/to.rb
+++ Casks/to.rb
@@ -1,6 +1,6 @@
 cask "to" do
-  version "1.3.0"
-  sha256 "96b1b0b58c76ba37b63af2476f36266a8d118510dd32a4024f12570fcc7fc2e0"
+  version "1.3.1"
+  sha256 "fcfe7c0eca89de5b30d7ca854c866ade6c612f657bdc505abdf20cfa50bcc611"
 
   url "https://github.com/SpectralKH/to/raw/master/dist/to-#{version}.tar.gz"
   name "to"
