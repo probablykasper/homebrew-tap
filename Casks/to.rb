c1110e2d9da17a50a0091e4710039f5214925011
Kasper <kasperkh.kh@gmail.com>
2020-01-29 03:26:05 +0100

    Updated homepage of to

--- Casks/to.rb
+++ Casks/to.rb
@@ -4,7 +4,7 @@ cask "to" do
 
   url "https://github.com/SpectralKH/to/raw/master/dist/to-#{version}.tar.gz"
   name "to"
-  homepage "https://github.com/spectralkh/to"
+  homepage "https://github.com/probablykasper/to"
 
   depends_on formula: "ffmpeg"
   depends_on formula: "imagemagick"
