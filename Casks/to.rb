b4f6d05ae883b8f4b1f060552013aaf5f3ec4a63
Kasper <kasperkh.kh@gmail.com>
2022-09-06 10:06:03 +0200

    to v1.5.0

--- Casks/to.rb
+++ Casks/to.rb
@@ -1,8 +1,8 @@
 cask "to" do
-  version "1.4.0"
-  sha256 "b36c9389b4d59af6393253059e4ffb5d87e8c38441aff1f3f9f4db20ee047cd6"
+  version "1.5.0"
+  sha256 "6010c6b49519434b7c0d8025d99ccca80aa992f43a02f0fb5478d39cb6067b96"
 
-  url "https://github.com/probablykasper/to/archive/refs/tags/#{version}.tar.gz"
+  url "https://github.com/probablykasper/to/archive/refs/tags/v#{version}.tar.gz"
   name "to"
   homepage "https://github.com/probablykasper/to"
 
