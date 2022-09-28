37e471ccf354f090c03c265254955f0715ccba6b
Kasper <kasperkh.kh@gmail.com>
2022-09-28 11:49:16 +0200

    Fix `to`

--- Formula/to.rb
+++ Formula/to.rb
@@ -1,15 +1,16 @@
-class Serve < Formula
+class To < Formula
   version "1.5.0"
-  sha256 "6010c6b49519434b7c0d8025d99ccca80aa992f43a02f0fb5478d39cb6067b96"
 
-  url "https://github.com/probablykasper/to/archive/refs/tags/v#{version}.tar.gz"
-  name "to"
+  url "https://github.com/probablykasper/to/raw/v#{version}/bin/to"
+  sha256 "71de86e685a3c97b53c7de37caf2d28fff92af0555becb503c95b97cd0ec89df"
+
+  desc "CLI audio, video and image file converter"
   homepage "https://github.com/probablykasper/to"
 
   depends_on "ffmpeg"
   depends_on "imagemagick"
 
   def install
-    bin.install "to-#{version}/bin/to"
+    bin.install "to"
   end
 end
