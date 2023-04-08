6d84c87f3e6d030d7b9183c9449244f9cc703369
Kasper <kasperkh.kh@gmail.com>
2023-04-08 18:25:42 +0200

    to v1.5.1

--- Formula/to.rb
+++ Formula/to.rb
@@ -1,8 +1,8 @@
 class To < Formula
-  version "1.5.0"
+  version "1.5.1"
 
   url "https://github.com/probablykasper/to/raw/v#{version}/bin/to"
-  sha256 "71de86e685a3c97b53c7de37caf2d28fff92af0555becb503c95b97cd0ec89df"
+  sha256 "f269bafc25a2b635e3399e9050ece594a23fb5f4ca863dd35c3faeae570c4500"
 
   desc "CLI audio, video and image file converter"
   homepage "https://github.com/probablykasper/to"
